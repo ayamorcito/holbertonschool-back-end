44e266e195c72f54040575278aea947296d7b77e
ayamorcito <izquierdoz.g@gmail.com>
2022-12-04 13:47:51 -0800

    1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
 """ Python script to export data in the CSV format. """
 
-
+import csv
 import requests
 from sys import argv
-import csv
 
 if __name__ == "__main__":
-    user_id = argv[1]
-    done_tasks = 0
-    total_tasks = 0
+    uid = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
+    user = requests.get(url).json()
 
-    user_tasks = requests.get(
-                'https://jsonplaceholder.typicode.com/users/{}/todos'
-                .format(user_id)).json()
-    user_name = requests.get(
-               'https://jsonplaceholder.typicode.com/users/{}'
-               .format(user_id)).json()['name']
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        uid)
+    todo = requests.get(url).json()
 
-    with open('{}.csv'.format(user_id), 'w') as csv_file:
-        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        
-        for task in user_tasks:
-            headers = [task.get('userId'),
-                       user_name.get('username'),
-                       task.get('completed'),
-                       task.get('title')]
-            writer.writerow(headers)
-    csv_file.close()
+    with open("{}.csv".format(uid), 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for t in todo:
+            taskwriter.writerow([int(uid), user.get('username'),
+                                 t.get('completed'),
+                                 t.get('title')])
